ee7fa5f65055fc46c8469e01a6d0f320f4400992
Danila Prokopenkov <DanWorkstation13@gmail.com>
2024-08-08 13:46:54 +0300

    hw-lesson03-01-done

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,7 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
+const average = calculateAverage(10, 20)
 
-function calculateAverage() {}
+function calculateAverage(num1, num2) {
+    return (num1 + num2) / 2;
+}
+console.log(average);
